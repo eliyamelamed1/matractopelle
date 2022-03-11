bcbb6f187f9c72bf0d1b19a75f8fc996f576a94f
eliyamelamed1 <eliyamelamed1@email.com>
2022-03-11 18:51:03 +0200

    added contact form and connected to EmailJs

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,21 +1,19 @@
-import { Button, FormControl, TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import React, { useRef, useState } from 'react';
 
 import MenuItem from '@mui/material/MenuItem';
+import emailjs from '@emailjs/browser';
 import { toast } from 'react-toastify';
 
 const currencies = [
     {
         value: 'Minipelle',
-        label: 'Minipelle',
     },
     {
         value: 'Pelle',
-        label: 'Pelle',
     },
     {
         value: 'Tractopelle',
-        label: 'Tractopelle',
     },
 ];
 
@@ -25,101 +23,53 @@ interface FormDataType {
     phone: string;
     city: string;
     item: string;
-    massage: string;
+    message: string;
     email: string;
 }
 
 const Contact = () => {
-    const [formData, setFormData] = useState<FormDataType>({
-        name: '',
-        zipCode: '',
-        phone: '',
-        city: '',
-        item: '',
-        massage: '',
-        email: '',
-    });
-
-    const { name, zipCode, email, phone, city, item, massage } = formData;
-
-    const onChange = (e) => setFormData((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
-    const onSubmit = (e) => {
+    const form = useRef();
+    const onSubmit = async (e) => {
         e.preventDefault();
-        for (const val of Object.values(formData)) {
-            if (val === '') return toast.error('Please fill all the inputs');
+
+        try {
+            await emailjs.sendForm('service_sknqxih', 'template_79ixiwh', form.current, 'JfIaPcyIDQHYxoOel');
+            toast.success('mail successfully sent');
+            form.current.reset();
+        } catch (err) {
+            toast.error('an error sending the text');
         }
-        // call send email api
     };
+
+    console.log(form.current);
 
     return (
         <div className='contact'>
-            <FormControl className='form'>
+            <form className='form' ref={form} onSubmit={onSubmit}>
                 <h1>Obtenez un devis</h1>
                 <h5>PARLEZ-NOUS DE VOS BESOINS, RÉPONSE IMMÉDIATE</h5>
-                <TextField
-                    label='Nom/Prénom'
-                    name='name'
-                    onChange={onChange}
-                    className='input'
-                    variant='standard'
-                    size='small'
-                />
-                <TextField
-                    label='Email'
-                    name='email'
-                    onChange={onChange}
-                    className='input'
-                    variant='standard'
-                    size='small'
-                />
+                <TextField label='Nom/Prénom' name='name' className='input' variant='standard' size='small' required />
+                <TextField label='Email' name='email' className='input' variant='standard' size='small' required />
                 <TextField
                     label='Code postal'
                     name='zipCode'
                     className='input'
                     variant='standard'
                     size='small'
-                    onChange={onChange}
+                    required
                 />
-                <TextField
-                    label='Téléphone'
-                    name='phone'
-                    className='input'
-                    variant='standard'
-                    size='small'
-                    onChange={onChange}
-                />
-                <TextField
-                    label='Votre ville'
-                    name='city'
-                    className='input'
-                    variant='standard'
-                    size='small'
-                    onChange={onChange}
-                />
-                <TextField
-                    select
-                    label='Type de grue souhaité'
-                    helperText=''
-                    variant='standard'
-                    name='item'
-                    onChange={onChange}
-                >
+                <TextField label='Téléphone' name='phone' className='input' variant='standard' size='small' required />
+                <TextField label='Votre ville' name='city' className='input' variant='standard' size='small' required />
+                <TextField defaultValue='' select label='Type de grue souhaité' variant='standard' name='item' required>
                     {currencies.map((option) => (
                         <MenuItem key={option.value} value={option.value}>
-                            {option.label}
+                            {option.value}
                         </MenuItem>
                     ))}
                 </TextField>
-                <TextField
-                    label='Message'
-                    variant='standard'
-                    multiline
-                    size='small'
-                    name='massage'
-                    onChange={onChange}
-                />
-                <Button onClick={onSubmit}>Envoyer</Button>
-            </FormControl>
+                <TextField label='Message' variant='standard' multiline size='small' name='message' />
+                <Button type='submit'>Envoyer</Button>
+            </form>
         </div>
     );
 };
