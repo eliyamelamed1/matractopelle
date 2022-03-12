60613dddf990f19261c18b9c77fc9905c22685b2
eliyamelamed1 <eliyamelamed1@email.com>
2022-03-12 14:46:15 +0200

    template finished

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,5 +1,5 @@
 import { Button, TextField } from '@mui/material';
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 import MenuItem from '@mui/material/MenuItem';
 import emailjs from '@emailjs/browser';
