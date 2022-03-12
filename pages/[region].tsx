1e80abf952a89b09ae497f37d907b27ad13cd5ce
eliyamelamed1 <eliyamelamed1@email.com>
2022-03-12 13:40:12 +0200

    added region sections

--- pages/[region].tsx
+++ pages/[region].tsx
@@ -4,6 +4,7 @@ import Items from '../components/Items';
 import type { NextPage } from 'next';
 import React from 'react';
 import { regions } from '../utils/enum';
+import { texts } from '../utils/texts';
 
 export async function getServerSideProps(context) {
     const region: string = context.params.region;
@@ -16,15 +17,45 @@ export async function getServerSideProps(context) {
     }
 }
 
-const region: NextPage<{ region: string }> = ({ region }) => {
-    return (
-        <div className='regions'>
-            <section>
-                <h1></h1>
-                <p></p>
+const generateSections = ({ region, company }: { region: string; company: string }) => {
+    const memo: number[] = [];
+    while (memo.length < 5) {
+        const randomNum = Math.floor(Math.random() * 100);
+        if (!memo.includes(randomNum)) memo.push(randomNum);
+    }
+
+    const sections = (
+        <>
+            <section key={memo[0]}>
+                <h1>{texts({ region, company })[memo[0]].title}</h1>
+                <p>{texts({ region, company })[memo[0]].paragraph}</p>
             </section>
-        </div>
+
+            <section key={memo[1]}>
+                <h1>{texts({ region, company })[memo[1]].title}</h1>
+                <p>{texts({ region, company })[memo[1]].paragraph}</p>
+            </section>
+
+            <section key={memo[2]}>
+                <h1>{texts({ region, company })[memo[2]].title}</h1>
+                <p>{texts({ region, company })[memo[2]].paragraph}</p>
+            </section>
+
+            <section key={memo[3]}>
+                <h1>{texts({ region, company })[memo[3]].title}</h1>
+                <p>{texts({ region, company })[memo[3]].paragraph}</p>
+            </section>
+        </>
     );
+
+    return sections;
+};
+
+const region: NextPage<{ region: string }> = ({ region }) => {
+    const company = 'company name';
+    const randomNum = Math.floor(Math.random() * 100);
+    const sections = generateSections({ region, company });
+    return <div className='region'>{sections}</div>;
 };
 
 export default region;
