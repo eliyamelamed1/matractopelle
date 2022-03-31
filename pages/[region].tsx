b62e7aca7ea5c8d1e2afcb27abe69c30e7590199
eliyamelamed1 <eliyamelamed1@email.com>
2022-03-31 17:16:06 +0300

    refactor

--- pages/[region].tsx
+++ pages/[region].tsx
@@ -24,32 +24,22 @@ export async function getServerSideProps(context) {
 
 const generateSections = ({ region, company }: { region: string; company: string }) => {
     const memo: number[] = [];
-    while (memo.length < 5) {
+    console.log(memo);
+    while (memo.length < 6) {
         const randomNum = Math.floor(Math.random() * 100);
         if (!memo.includes(randomNum)) memo.push(randomNum);
     }
 
     const sections = (
         <div className='sections'>
-            <section key={memo[0]}>
-                <h1>{texts({ region, company })[memo[0]].title}</h1>
-                <p>{texts({ region, company })[memo[0]].paragraph}</p>
-            </section>
-
-            <section key={memo[1]}>
-                <h1>{texts({ region, company })[memo[1]].title}</h1>
-                <p>{texts({ region, company })[memo[1]].paragraph}</p>
-            </section>
-
-            <section key={memo[2]}>
-                <h1>{texts({ region, company })[memo[2]].title}</h1>
-                <p>{texts({ region, company })[memo[2]].paragraph}</p>
-            </section>
-
-            <section key={memo[3]}>
-                <h1>{texts({ region, company })[memo[3]].title}</h1>
-                <p>{texts({ region, company })[memo[3]].paragraph}</p>
-            </section>
+            {memo.map((item) => {
+                return (
+                    <section key={item}>
+                        <h1>{texts({ region, company })[item].title}</h1>
+                        <p>{texts({ region, company })[item].paragraph}</p>
+                    </section>
+                );
+            })}
         </div>
     );
 
